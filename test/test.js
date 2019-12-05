d9ccf230991b30ae7016843c8fbc84a7fe900c78
kamranjon <kamran.pakseresht@udacity.com>
2019-12-05 07:37:39 -0800

    Add test for proposed solution

--- test/test.js
+++ test/test.js
@@ -1,20 +1,36 @@
 var bunyan = require('bunyan')
 var chai = require('chai')
 var sinon = require('sinon')
-var sandbox = sinon.createSandbox();
 var sinonChai = require('sinon-chai');
 var expect = chai.expect;
 chai.use(sinonChai);
 
 describe('bunyan logger', () => {
-  it('does not leak env vars', () => {
+  var sandbox = sinon.createSandbox();
+  
+  beforeEach(() => {
     sandbox.spy(process.stderr, 'write');
+  })
 
+  afterEach(() => {
+    process.stderr.write.restore();
+  })
+
+  it('does leak env vars', () => {    
     const logger = bunyan.createLogger({name: 'test'});
     const info = logger.info;
 
     info('Log message which will output `this` to stderr');
 
-    expect(process.stderr.write).not.to.have.been.calledWithMatch(/env:/);
+    expect(process.stderr.write).to.have.been.calledWithMatch(/env:/);
   });
+  it('does not leak env vars if we bind early', () => {
+    const logger = bunyan.createLogger({name: 'test'});
+    logger.info.bind({reason: 'prevent-leak'})('Log once with a safe `this` value which prevents future output');
+    const info = logger.info;
+
+    info('Log message which will output `this` to stderr');
+
+    expect(process.stderr.write).not.to.have.been.calledWithMatch(/env:/);
+  })
 });
